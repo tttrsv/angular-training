dcd3d84562e0440527d1a5e142d3cd15c7658891
tttrsv <tttrsv@gmail.com>
2019-12-08 11:43:52 +0200

    task with events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
     
 public visible: boolean;
+public timerStarted: boolean;
+public title = "App Component Title";
+public message:string;
+timeLeft: number = 10;
+interval;
     
 ngOnInit() {
-        this.visible = true;
+        this.visible = false;
   }
     
 toggle(){
     this.visible = !this.visible;
 }
+  
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      if(this.timeLeft > 0) {
+        this.timerStarted = true;
+        this.timeLeft--;
+      } else{
+        this.timerStarted = false;
+          this.message = 'Timer finished';
+      }
+    },1000)
+  }
+
+  pauseTimer() {
+    clearInterval(this.interval);
+  }
+
+  displayContent(visible:boolean){
+    this.visible = visible;
+  }
     
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,34 @@ import { SuccessModule } from './components/success/success.module';
 import { WarningModule } from './components/warning/warning.module';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import { CardComponent } from './components/card/card.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MessageComponent } from './components/message/message.component';
+import { TitleComponent } from './components/title/title.component';
+import { FormsModule } from '@angular/forms';
+import { ContentComponent } from './components/content/content.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    MessageComponent,
+    TitleComponent,
+    ContentComponent
     ],
   imports: [
     BrowserModule,
     SuccessModule,
-    WarningModule
+    WarningModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatGridListModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
